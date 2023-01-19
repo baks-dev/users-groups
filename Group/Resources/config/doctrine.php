5e7cd71be5f69ca44023beb798e12d0e5c96d484
baks.dev <admin@baks.dev>
2023-01-20 00:54:47 +0300

    Baks Development

--- Group/Resources/config/doctrine.php
+++ Group/Resources/config/doctrine.php
@@ -13,21 +13,21 @@ use BaksDev\Users\Groups\Group\Type\Settings\GroupSettingsType;
 
 use Symfony\Config\DoctrineConfig;
 
-return static function (DoctrineConfig $doctrine)
-{
-
-    $doctrine->dbal()->type(GroupPrefix::TYPE)->class(GroupPrefixType::class);
-    $doctrine->dbal()->type(GroupEventUid::TYPE)->class(GroupEventUidType::class);
-    $doctrine->dbal()->type(GroupCheckUid::TYPE)->class(GroupCheckType::class);
-    $doctrine->dbal()->type(GroupSettings::TYPE)->class(GroupSettingsType::class);
-    
-    $emDefault = $doctrine->orm()->entityManager('default');
-    
-    $emDefault->autoMapping(true);
-    $emDefault->mapping('UserGroup')
-      ->type('attribute')
-      ->dir('%kernel.project_dir%/src/Module/Users/Groups/Group/Entity')
-      ->isBundle(false)
-      ->prefix('BaksDev\Users\Groups\Group\Entity')
-      ->alias('UserGroup');
+return static function(DoctrineConfig $doctrine){
+	
+	$doctrine->dbal()->type(GroupPrefix::TYPE)->class(GroupPrefixType::class);
+	$doctrine->dbal()->type(GroupEventUid::TYPE)->class(GroupEventUidType::class);
+	$doctrine->dbal()->type(GroupCheckUid::TYPE)->class(GroupCheckType::class);
+	$doctrine->dbal()->type(GroupSettings::TYPE)->class(GroupSettingsType::class);
+	
+	$emDefault = $doctrine->orm()->entityManager('default');
+	
+	$emDefault->autoMapping(true);
+	$emDefault->mapping('UserGroup')
+		->type('attribute')
+		->dir(__DIR__.'/../../Entity')
+		->isBundle(false)
+		->prefix('BaksDev\Users\Groups\Group\Entity')
+		->alias('UserGroup')
+	;
 };
