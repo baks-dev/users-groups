24605dc0a84f722c5aef3bfc7d4ed848c1f47dc3
kingstar <admin@baks.dev>
2023-06-26 21:53:05 +0300

    Baks Development

---
+++ Users/Controller/Admin/Tests/DeleteControllerTest.php
@@ -0,0 +1,154 @@
+<?php
+/*
+ *  Copyright 2022.  Baks.dev <admin@baks.dev>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
+namespace BaksDev\Users\Groups\Users\Controller\Admin\Tests;
+
+use BaksDev\Users\Groups\Users\Entity\CheckUsers;
+use BaksDev\Users\Groups\Users\Type\Event\CheckUsersEventUid;
+use BaksDev\Users\User\Tests\TestUserAccount;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * @group users-groups
+ */
+final class DeleteControllerTest extends WebTestCase
+{
+    private const URL = '/admin/user/check/delete/%s';
+
+    private const ROLE = 'ROLE_CHECK_USERS_DELETE';
+
+    private static ?CheckUsersEventUid $identifier;
+
+    public static function setUpBeforeClass(): void
+    {
+        $em = self::getContainer()->get(EntityManagerInterface::class);
+        self::$identifier = $em->getRepository(CheckUsers::class)->findOneBy([], ['id' => 'DESC'])?->getEvent();
+    }
+
+
+
+    /** Доступ по роли */
+    public function testRoleProductSuccessful(): void
+    {
+        // Получаем одно из событий
+        $Event = self::$identifier;
+
+        if ($Event)
+        {
+            self::ensureKernelShutdown();
+            $client = static::createClient();
+
+            foreach (TestUserAccount::getDevice() as $device)
+            {
+                $client->setServerParameter('HTTP_USER_AGENT', $device);
+
+                $user = TestUserAccount::getModer(self::ROLE);
+
+                $client->loginUser($user, 'user');
+                $client->request('GET', sprintf(self::URL, $Event->getValue()));
+
+                self::assertResponseIsSuccessful();
+            }
+        } else
+        {
+            self::assertTrue(true);
+        }
+    }
+
+    // доступ по роли ROLE_ADMIN
+    public function testRoleAdminSuccessful(): void
+    {
+        // Получаем одно из событий
+        $Event = self::$identifier;
+
+        if ($Event)
+        {
+            self::ensureKernelShutdown();
+            $client = static::createClient();
+
+            foreach (TestUserAccount::getDevice() as $device)
+            {
+                $client->setServerParameter('HTTP_USER_AGENT', $device);
+
+                $user = TestUserAccount::getAdmin();
+
+                $client->loginUser($user, 'user');
+                $client->request('GET', sprintf(self::URL, $Event->getValue()));
+
+                self::assertResponseIsSuccessful();
+            }
+        } else
+        {
+            self::assertTrue(true);
+        }
+    }
+
+    // доступ по роли ROLE_USER
+    public function testRoleUserDeny(): void
+    {
+        // Получаем одно из событий
+        $Event = self::$identifier;
+
+        if ($Event)
+        {
+            self::ensureKernelShutdown();
+            $client = static::createClient();
+
+            foreach (TestUserAccount::getDevice() as $device)
+            {
+                $client->setServerParameter('HTTP_USER_AGENT', $device);
+
+                $user = TestUserAccount::getUser();
+                $client->loginUser($user, 'user');
+                $client->request('GET', sprintf(self::URL, $Event->getValue()));
+
+                self::assertResponseStatusCodeSame(403);
+            }
+        } else
+        {
+            self::assertTrue(true);
+        }
+    }
+
+    /** Доступ по без роли */
+    public function testGuestFiled(): void
+    {
+        // Получаем одно из событий
+        $Event = self::$identifier;
+
+        if ($Event)
+        {
+            self::ensureKernelShutdown();
+            $client = static::createClient();
+
+            foreach (TestUserAccount::getDevice() as $device)
+            {
+                $client->setServerParameter('HTTP_USER_AGENT', $device);
+
+                $client->request('GET', sprintf(self::URL, $Event->getValue()));
+
+                // Full authentication is required to access this resource
+                self::assertResponseStatusCodeSame(401);
+            }
+        } else
+        {
+            self::assertTrue(true);
+        }
+    }
+}
